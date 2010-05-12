4d49991741e88b73368f17ea9741e303c7c88926
MIYAMOTO Daisuke <dai.0304@gmail.com>
2010-05-12 14:58:10 +0000

    [COM-7] 簡単だが、package-info.javaを追加。

---
+++ src/main/java/org/jiemamy/utils/visitor/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * {@link java.util.Collection}や{@link java.util.Map}等、複数の要素を持つObjectに対して、
+ * 全ての要素に処理を行うためのユーティリティ {@link org.jiemamy.utils.visitor.ForEachUtil} を中心に、
+ * その処理器の抽象クラスを提供するパッケージ。
+ * 
+ * @author daisuke
+ */
+package org.jiemamy.utils.visitor;
+
